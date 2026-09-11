ecfda9db2b2914ba090571347e2fa088b325da12
Eklavya <rajeklavya65@gmail.com>
2026-09-11 09:10:16 +0530

    BitManipulation added

---
+++ BitManipulation.java
@@ -0,0 +1,102 @@
+public class BitManipulation {
+    public static void main(String args[]){
+        // oddOrEven(3);
+        // oddOrEven(2);
+        // oddOrEven(19);
+        // System.out.println(getIthBit(10, 2));
+        // System.out.println(setIthBit(10, 2));
+        // System.out.println(clearIthBit(10, 1));
+        // System.out.println(updateIthBit(10, 2, 1));
+        // System.out.println(clearIbits(15,2));
+        // System.out.println(clearRangeIbits(15, 1, 2));
+        // System.out.println(countSetBits(15));
+        // System.out.println(fastExpo(5, 10));
+    }
+
+
+
+    public static void oddOrEven(int n){
+        int bitMask = 1;
+        if((n & bitMask) == 0){
+            //even number
+            System.out.println("Even");
+        }else{
+            //odd
+            System.out.println("Odd");
+        }
+    }
+
+    public static int getIthBit(int n, int i){
+        int bitMask = 1<<i;
+        if((n & bitMask) == 0){
+            return 0;
+        }else{
+            return 1;
+        }
+    }
+
+    public static int setIthBit(int n, int i){
+        int bitMask = 1<<i ;
+        return (n|bitMask) ;
+    }
+
+    public static int clearIthBit(int n, int i){
+        int bitMask = 1<<i;
+        return (n & ~bitMask);
+    }
+
+    public static int updateIthBit(int n, int i , int newBit){
+        if(newBit == 0){
+            return clearIthBit(n, i);
+        }else{
+            return setIthBit(n,i) ;
+        }
+    }
+
+    public static int clearIbits(int n, int i){
+        int bitMask = (-1<<i);
+        return n & bitMask ;
+    }
+
+    public static int clearRangeIbits(int n, int i, int j){
+        int a = (-1<<(j+1));
+        int b = (1<<i)-1;
+        int bitMask = a | b;
+        return n & bitMask;
+    }
+
+    public static int countSetBits(int n){
+        int count = 0;
+        while(n>0){
+            if((n & 1) != 0){
+                count++;
+            }
+            n = n>>1;
+        }
+        return count;
+    }
+
+    public static int fastExpo(int a, int n){
+        int ans = 1;
+        while(n>0){
+            if((n & 1) != 0){
+                ans = ans * a;
+            }
+            a = a * a;
+            n = n>>1;
+        }
+        return ans;
+    }
+
+    public static int moduloExpo(int a, int n, int p){
+        int ans = 1;
+        while(n>0){
+            if((n & 1) != 0){
+                ans = (ans * a) % p;
+            }
+            a = (a * a) % p;
+            n = n>>1;
+        }
+        return ans;
+    }
+}
